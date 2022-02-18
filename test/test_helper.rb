a596e7d3ad6bfccb260abaaf9dd45e95f66e92b9
sakataku1991 <sakataku1991@gmail.com>
2022-02-18 21:07:03 +0900

    Railsのテストの設定が完了

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,19 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+# gem minitest-reporters setup
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+
+  # 並列テスト ... 複数のプロセスを分岐させテスト時間の短縮を行なう機能
+  # プロセスが分岐した直後に呼び出し
+  parallelize_setup do |worker|
+    # seedデータの読み込み
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -10,4 +22,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
 end
