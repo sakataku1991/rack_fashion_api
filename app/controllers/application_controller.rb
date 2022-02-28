983f6963c1aaa60c4556034494b2704983507ddc
sakataku1991 <sakataku1991@gmail.com>
2022-02-28 17:37:59 +0900

    SSLリダイレクト処理の設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::API
 
+  # 301リダイレクト（本番環境のみ有効）
+  before_action :moved_permanently, if: :is_redirect
   # Cookieを扱う
   include ActionController::Cookies
   # 認可を行なう
@@ -22,6 +24,18 @@ class ApplicationController < ActionController::API
     # Internal Server Error
     def response_500(msg = "Internal Server Error")
       render status: 500, json: { status: 500, error: msg }
+    end
+
+    # リダイレクト条件に一致した場合はtrueを返す
+    # TODO すべてのURLのリダイレクト => Gemの「rack-rewrite」で実装できる！
+    def is_redirect
+      redirect_domain = "herokuapp.com"
+      Rails.env.production? && ENV["BASE_URL"] && request.host.include?(redirect_domain)
+    end
+
+    # 301リダイレクトを行う
+    def moved_permanently
+      redirect_to "#{ENV["BASE_URL"]}#{request.path}", status: 301
     end
 
 end
