681ece2a7ee801383e626ca15cad9e1b5ab1a612
sakataku1991 <sakataku1991@gmail.com>
2022-03-05 16:00:22 +0900

    sexesテーブルの「gender」カラムの名前を「name」に変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_013727) do
+ActiveRecord::Schema.define(version: 2022_03_05_065819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2022_03_04_013727) do
   end
 
   create_table "sexes", force: :cascade do |t|
-    t.string "gender", null: false
+    t.string "name", null: false
     t.string "code", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
